d31e3dfaf5f050080efcf7209b123d91f4fad941
utnso <zakerik10@gmail.com>
2024-04-05 21:01:53 +0000

    Se comienza a codear en server, TODAVIA NO TERMINADO

--- server/src/server.c
+++ server/src/server.c
@@ -2,7 +2,6 @@
 
 int main(void) {
 	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
-
 	int server_fd = iniciar_servidor();
 	log_info(logger, "Servidor listo para recibir al cliente");
 	int cliente_fd = esperar_cliente(server_fd);
